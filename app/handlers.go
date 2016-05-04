ac128b91dc8b46d51b63b5ddcdbcf4898be54f85
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-05-04 02:02:43 +0200

    Adds info message to backend index

--- app/handlers.go
+++ app/handlers.go
@@ -4,9 +4,20 @@ import (
   "fmt"
   "net/http"
 )
+
+var (
+	version = `======================================
+= ArmIT Backend: v0.0                =
+=                                    =
+= Alexander Håkansson                =
+= alexander@hakansson.xyz            =
+= github.com/hsson/armit-website     =
+======================================`
+)
+
 // GET: /
 func Index(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Welcome to armit backend!")
+  fmt.Fprintf(w, version)
 }
 
 // GET: /secured
